58356d717ba61a0d5a4fc058637689f5cd331da0
Unknown <kaizerjd@gmail.com>
2017-02-14 20:39:42 -0600

    Initial Commit

---
+++ CalendarDayPlanner/CalendarDayPlanner.h
@@ -0,0 +1,19 @@
+//
+//  CalendarDayPlanner.h
+//  CalendarDayPlanner
+//
+//  Created by JD Bartee on 2/14/17.
+//  Copyright © 2017 JD Bartee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalendarDayPlanner.
+FOUNDATION_EXPORT double CalendarDayPlannerVersionNumber;
+
+//! Project version string for CalendarDayPlanner.
+FOUNDATION_EXPORT const unsigned char CalendarDayPlannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarDayPlanner/PublicHeader.h>
+
+
